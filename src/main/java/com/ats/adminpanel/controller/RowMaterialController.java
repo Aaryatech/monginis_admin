014c45f661842dd8cfe6c09262e115786425519e
Viplav Thoke <viplavthoke@gmail.com>
2017-11-21 10:15:32 +0530

    RowMaterialController  //Ganesh

---
+++ src/main/java/com/ats/adminpanel/controller/RowMaterialController.java
@@ -0,0 +1,40 @@
+package com.ats.adminpanel.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ats.adminpanel.commons.Constants;
+import com.ats.adminpanel.model.RawMaterial.Info;
+import com.ats.adminpanel.model.RawMaterial.SupplierDetails; 
+
+
+@Controller
+public class RowMaterialController {
+	
+	
+	@RequestMapping(value = "/showRowMaterial", method = RequestMethod.GET)
+	public ModelAndView showProdForcasting(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("masters/rowMaterialMaster");
+
+
+		return model;
+	}
+	
+	
+	@RequestMapping(value = "/showRmRateVerification", method = RequestMethod.GET)
+	public ModelAndView showRmRateVerification(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("masters/rmRateVerification");
+
+
+		return model;
+	}
+
+}
