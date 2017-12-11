be3da0e5d32d9aff9d824c7bed31b650d536a7e2
Aaryatech <code@atsinfosoft.com>
2017-12-11 15:28:53 +0530

    CommonConf//Mahesh

--- src/main/java/com/ats/adminpanel/model/franchisee/CommonConf.java
+++ src/main/java/com/ats/adminpanel/model/franchisee/CommonConf.java
@@ -3,6 +3,22 @@ package com.ats.adminpanel.model.franchisee;
 public class CommonConf {
 private int id;
 private String name;
+private int qty;
+private int rmUomId;
+
+
+public int getRmUomId() {
+	return rmUomId;
+}
+public void setRmUomId(int rmUomId) {
+	this.rmUomId = rmUomId;
+}
+public int getQty() {
+	return qty;
+}
+public void setQty(int qty) {
+	this.qty = qty;
+}
 public int getId() {
 	return id;
 }
@@ -17,7 +33,8 @@ public void setName(String name) {
 }
 @Override
 public String toString() {
-	return "CommonConf [id=" + id + ", name=" + name + "]";
+	return "CommonConf [id=" + id + ", name=" + name + ", qty=" + qty + "]";
 }
+
 
 }
