60b68d346aa30927ebf90e3c37208e99424d0114
Aaryatech <code@atsinfosoft.com>
2017-12-14 16:36:30 +0530

    Sp Production Masters//Mahesh

---
+++ src/main/java/com/ats/adminpanel/model/spprod/Employee.java
@@ -0,0 +1,87 @@
+package com.ats.adminpanel.model.spprod;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable{
+
+
+	private boolean isError;
+	
+	private String message;
+	
+	private int empId;
+	
+	private String empName;
+
+	private int deptId;
+
+	private int isUsed;
+	
+	private int delStatus;
+
+	private int empType;
+	
+	public boolean isError() {
+		return isError;
+	}
+
+	public void setError(boolean isError) {
+		this.isError = isError;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+
+	public int getIsUsed() {
+		return isUsed;
+	}
+
+	public void setIsUsed(int isUsed) {
+		this.isUsed = isUsed;
+	}
+
+	public int getDelStatus() {
+		return delStatus;
+	}
+
+	public void setDelStatus(int delStatus) {
+		this.delStatus = delStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [isError=" + isError + ", message=" + message + ", empId=" + empId + ", empName=" + empName
+				+ ", deptId=" + deptId + ", isUsed=" + isUsed + ", delStatus=" + delStatus + "]";
+	}
+    
+    
+}
