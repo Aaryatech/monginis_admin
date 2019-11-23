be9ab5e33770fdee7c7834e6c644464f8b6644cb
maddy <maddy@maddy-Inspiron-3541>
2019-11-23 19:19:12 +0530

    //maddy

--- src/main/java/com/ats/adminpanel/model/item/MCategoryList.java
+++ src/main/java/com/ats/adminpanel/model/item/MCategoryList.java
@@ -9,7 +9,7 @@ public class MCategoryList {
     private String catName;
     private Integer isSameDay;
     private Integer delStatus;
-    private List<SubCategory> subCategory = null;
+    private List<SubCategory> subCategoryList = null;
 
     public Integer getCatId() {
         return catId;
@@ -43,21 +43,19 @@ public class MCategoryList {
         this.delStatus = delStatus;
     }
 
-    public List<SubCategory> getSubCategory() {
-        return subCategory;
-    }
+	public List<SubCategory> getSubCategoryList() {
+		return subCategoryList;
+	}
 
-    public void setSubCategory(List<SubCategory> subCategory) {
-        this.subCategory = subCategory;
-    }
+	public void setSubCategoryList(List<SubCategory> subCategoryList) {
+		this.subCategoryList = subCategoryList;
+	}
 
 	@Override
 	public String toString() {
 		return "MCategoryList [catId=" + catId + ", catName=" + catName + ", isSameDay=" + isSameDay + ", delStatus="
-				+ delStatus + ", subCategory=" + subCategory + "]";
+				+ delStatus + ", subCategoryList=" + subCategoryList + "]";
 	}
 
-    
-    
-    
+  
 }
